d35a0ac887089823b582e30adc04bda2d29c2789
alvaro <alvarorivera@gmail.com>
2022-05-30 18:24:43 -0500

    estructura del proyecto

---
+++ model/database.php
@@ -0,0 +1,33 @@
+<?php
+    class DB{
+        private $host;
+        private $db;
+        private $user;
+        private $password;
+        private $charset;
+
+        public function __construct(){
+            $this->host = 'localhost';
+            $this->db = 'hotel';
+            $this->user = 'root';
+            $this->password = '';
+            $this->charset = 'utf8mb4';
+        }
+
+        public function connect(){
+            
+            try{
+                $options = [
+                    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_EMULATE_PREPARES   => false,
+                ];
+                $connection=new PDO("mysql:host=$this->host; dbname=$this->db;charset=$this->charset",$this->user,$this->password,$options);
+                return $connection;
+            }catch(PDOException $e){
+                die("Error connection". $e->getMessage);
+                
+
+            }
+
+        }
+    }
