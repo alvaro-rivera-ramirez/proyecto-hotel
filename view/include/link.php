787a50a594cf4af76b8db15e87ed78aab5e3d7e7
alvaro <alvarorivera@gmail.com>
2022-05-31 00:15:41 -0500

    Agregando vistas parte2

---
+++ view/include/link.php
@@ -0,0 +1,4 @@
+    <link href="css/botsstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/icon/fontello.css" rel="stylesheet">
+    <link href="css/fontawesome/css/all.min.css" rel="stylesheet">
--- view/reserva_hab.php
+++ view/reserva_hab.php
@@ -4,143 +4,17 @@
     <meta charset="UTF-8">
     <title>Hotel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link href="css/botsstrap/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/icon/fontello.css" rel="stylesheet">
-    <link href="css/fontawesome/css/all.min.css" rel="stylesheet">
+    <?php include "include/link.php" ?>
 </head>
 <body>
    <div class="d-flex">
-       <div id="sidebar-container">
-            
-            <!---- ofcanvas--->
-        <div class="offcanvas offcanvas-start canvas-menu" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-         <div class="logo align-items-center bg-primary">
-               <img src="Imagenes/usuario.png">
-                <p class="text-center text-light font-weight-bold">Administrador</p>
-            </div>
-            <div class="menu">
-               <div class="modulos">
-                   <h4>Módulos</h4>
-               </div>
-                <!--------------navegación-------->
-                <a href="#" class="d-block" ><i class="icon-home me-3"></i>Inicio</a>
-                <a href="#" class="d-block"><i class="icon-calendar me-3"></i>Reservar</a>
-                <a href="#" class="d-block" ><i class="icon-user-add me-3"></i>Registrar Cliente</a>
-                
-                <div class="accordion accordion-flush" id="accordionFlushExample">
-                      <div class="accordion-item">
-                          <button class="" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            <i class="icon-clipboard me-3"></i>
-                            Registros
-                            <i class="icon-down-open-big despliegue"></i>
-                          </button>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                          <div class="sub-menu">
-                            <a href="#" class="d-block">
-                              <i class="fa-solid fa-bell-concierge"></i>
-                              Reservas</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-users"></i>
-                              Clientes</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-users-gear"></i>
-                              Empleados</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-bed"></i>
-                              Habitaciones</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-tag"></i>
-                              Tipo de Habitacion</a>
-                            </div>
-                        </div>
-                      </div>
-                      <div class="accordion-item">
-                          <button class="" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                           <i class="icon-chart-bar me-3"></i> Reportes
-                           <i class="icon-down-open-big despliegue"></i>
-                          </button>
-                        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                          <div class="sub-menu">
-                            
-                             <a href="#" class="d-block">
-                              <i class="fa-solid fa-calendar-day"></i>
-                                Dia</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-calendar-days"></i>
-                              Mes</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-users"></i>
-                              Clientes</a>
-                              <a href="#" class="d-block">
-                              <i class="fa-solid fa-hotel"></i>
-                              Habitaciones</a>
-                            </div>
-                        </div>
-                      </div>
-                      <div class="accordion-item">
-                          <button class="" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            <i class=" icon-tools me-3"></i>Configuración
-                            <i class="icon-down-open-big despliegue"></i>
-                          </button>
-                        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                          <div class="sub-menu">
-                              
-                              <a href="#" class="d-block">
-                              <i class="icon-user"></i>
-                                Perfil</a>
-                              <a href="#" class="d-block">
-                              <i class="icon-key"></i>
-                               Contraseña</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                <a href="#" class="d-block"><i class="icon-logout me-3"></i>Salir</a>
-            </div>
-        </div>
-
-        <!----------------->
-        </div>
-        
+        <?php include "include/navLateral.php"?>     
        
         <div class="w-100" style="background-color: #F1F3F5">
-           <!---- navbar ----->
-            <nav class="navbar navbar-dark bg-dark navbar-expand-lg bg-primary pe-2">
-              <div class="container-fluid">
-               <!--- canvas trigger --->
-               <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample" >
-                  
-                  <span class="navbar-toggler-icon"></span>
-                </button>
+            
+            <?php include "include/navBar.php"?>
 
-               <!------------------------>
-                <a class=" text-light navbar-brand" href="#">Hotel Montereal</a>
-                <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    
-                    <li class="nav-item dropdown">
-                      <a class="  text-light nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                       <img src="Imagenes/usuario.png" width="30px" class="me-2">
-                        Dario Gomez
-                      </a>
-                      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Perfil</a></li>
-                        <li><a class="dropdown-item" href="#">Configuración</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Salir</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-            <!---->
-            <!----->
+            <!--------------CONTENIDO-------------->
             <div class="ps-2 pt-3 content-body">
                   <div class="content p-5 pt-1" >
                        <h3 class="pb-2">Reservar Habitación</h3>
@@ -256,7 +130,6 @@
                 </div>
         </div>
    </div>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery-3.6.0.min.js"></script>
+    <?php include "include/script.php"?>
 </body>
 </html>
