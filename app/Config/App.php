5949d8257ae49df57a9e92fe26ef8097d62d694a
alvarorivera <alvarorivera2001@gmail.com>
2022-06-25 13:29:14 -0500

    ingresando archivos del framework

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/proyectoIS/public/';
+    public $baseURL = 'http://localhost/proyecto-hotel/public/';
 
     /**
      * --------------------------------------------------------------------------
